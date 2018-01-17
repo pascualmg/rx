455d4125728cea26bca962f7cc3e85ecf2e36d9a
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-01-17 15:37:58 +0100

    ejercicio 02

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -22,6 +22,8 @@ var ejercicios = {
         console.clear();
         const source = ['1', '10', 'foo', '2', '3', '5', 'bar', '8', '13'];
 
+        var result = source.filter(function OnlyNumbers(item){Number(item)})
+        ;
         console.log(result);
 
         /* output
