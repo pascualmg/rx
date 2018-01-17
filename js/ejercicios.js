4c5065738dadd80430c00d9ecbb56ed752ee85c9
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-01-17 14:50:54 +0100

    ejercicio 01

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -2,14 +2,17 @@
 var ejercicios = {
 
     // Crea una variable "result" que contenga la suma
-    // de todos los números en "source". Ua un for loop tradicional
+    // de todos los números en "ource". Ua un for loop tradicional
     // para este ejercicio
     ejer01: function ejer01() {
         console.clear();
         const source = ['1', '10', 'foo', '2', '3', '5', 'bar', '8', '13'];
 
 
-        for(i = 0,result=0;i< source.length;i++, result+=Number(source[i]) === NaN?0:Number(source[i]) ){};
+        for(i = 0,result=0;i< source.length;i++, result+=Number(source[i]) === NaN?0:Number(source[i]) ){
+           console.log(source[i]);
+            console.log(Number(source[i]));
+        };
 
         console.log(result);
 
