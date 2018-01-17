cf4fc46d9891bfa80286eccc48865f39526e03c2
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-01-17 15:46:59 +0100

    ejercicio 02

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -22,7 +22,9 @@ var ejercicios = {
         console.clear();
         const source = ['1', '10', 'foo', '2', '3', '5', 'bar', '8', '13'];
 
-        var result = source.filter(function OnlyNumbers(item){return Number(item);})
+        var result = source
+            .filter(function predicateonlyNumbers(item){return Number(item);})
+            .reduce(function combinerSummation(acc, curr){return acc + curr;})
         ;
         console.log(result);
 
