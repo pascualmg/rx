4d20c722a0a9ed5e9f662a6c5b467e8f23a3cf7f
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-01-17 14:34:52 +0100

    ejercicio 01

---
+++ js/ejercicios.js
@@ -0,0 +1,21 @@
+// ~~~~~~ 1
+var ejercicios = {
+
+    // Crea una variable "result" que contenga la suma
+    // de todos los números en "source". Ua un for loop tradicional
+    // para este ejercicio
+    ejer01: function ejer01() {
+        console.clear();
+        const source = ['1', '10', 'foo', '2', '3', '5', 'bar', '8', '13'];
+
+
+        for(i = 0,result=0;i< source.length;i++, result+=source[i]) {};
+
+        console.log(result);
+
+
+        /* output
+        42
+        */
+    }
+};
