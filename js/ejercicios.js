424beddbaef09fffe66127d580629617a68cbbb3
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-01-17 15:27:52 +0100

    ejercicio 01

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,3 +1,4 @@
+"use strict"
 var ejercicios = {
     // Crea una variable "result" que contenga la suma
     // de todos los números en "ource". Ua un for loop tradicional
@@ -5,11 +6,14 @@ var ejercicios = {
     ejer01: function ejer01() {
         console.clear();
         const source = ['1', '10', 'foo', '2', '3', '5', 'bar', '8', '13'];
-
-        for(let i = 0, result = 0;i< source.length;result += Number(source[i])?Number(source[i]):0,i++ ){ };
+        var result = 0;
+        for(let i = 0;i< source.length;result += Number(source[i])?Number(source[i]):0,i++ ){ };
         console.log(result);
         /* output
         42
         */
+    },
+    ejer02: function ejer02() {
+
     }
 };
