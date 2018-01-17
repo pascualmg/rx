27418ef2099692407fb5e2e04be972e849f414a1
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-01-17 14:52:40 +0100

    ejercicio 01

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -9,7 +9,7 @@ var ejercicios = {
         const source = ['1', '10', 'foo', '2', '3', '5', 'bar', '8', '13'];
 
 
-        for(i = 0,result=0;i< source.length;i++, result+=Number(source[i]) === NaN?0:Number(source[i]) ){
+        for(i = 0,result=0;i< source.length-1;i++, result+=Number(source[i]) === "NaN"?0:Number(source[i]) ){
            console.log(source[i]);
             console.log(Number(source[i]));
         };
