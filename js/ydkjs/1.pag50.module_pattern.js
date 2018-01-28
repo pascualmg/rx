7d256a29a0efa6dfef05bb58ab6fce4b9093bd54
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-01-28 01:08:02 +0100

    ydkjs , module pattern using scope closure

---
+++ js/ydkjs/1.pag50.module_pattern.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function User(){
+    let publicAPI;
+    let username, password;
+
+    function doLogin(user, pass) {
+        username = user;
+        password = pass;
+        //do the rest of the login work.
+    }
+
+    function showData() {
+       console.log('me llamo ', username);
+       console.log('tengo el pass ', password);
+    }
+
+    publicAPI = {
+        login: doLogin,
+        showData: showData,
+    };
+
+    return publicAPI; 
+}
+
+//create a user module instance.
+debugger;
+var passh = User();
+
+passh.login('passh', 'capullo100');
+
+passh.showData();
+
+
