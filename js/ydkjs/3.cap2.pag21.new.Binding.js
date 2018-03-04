8d21404d0b5087f4eebccae268761f5c555d1e22
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-03-04 20:13:03 +0100

    sabado medio noche default binding , el final.

--- js/ydkjs/3.cap2.pag21.new.Binding.js
+++ js/ydkjs/3.cap2.pag21.new.Binding.js
@@ -16,8 +16,8 @@
  */
 
 /**
- * Intento de traducción de la pag 21 del libro , explicando
- * la cuarta regla.
+ * Intento de traducción del 3 libro, capitulo 2 ,  pag 21 ydkjs de kyle Sympson , explicando
+ * la cuarta regla del bindeo del this.
  *
  * La cuarta regla del 'this binding'requiere tener que repensar
  * uno algo con lo que todo la mayoría no se entera en lo
@@ -45,14 +45,40 @@
  * Ni siquiera son funciones especiales, son simples y normales funciones en
  * ensencia ´secuestradas´ por el uso del new en la invocación.
  *
+ * Por ejemplo , consideremos la Number()
  *
- *
- *
- *
- *
- *
- *
+ * que según las specs que hay ahora mismo en ES5:
+ *  --Cuando Number() se invoca con el new como parte de una expresión
+ *    entonces es un constructor. Inicializa un objeto totalmente nuevo.
  */
+
+var miNumero = new Number("1200.23");
+
+console.log('miNumero', miNumero);//[Number: 1200.23]
+console.log('typeof miNumero', typeof miNumero);//Object
+
+
+/**
+ * Así que la mayoría de las funciones viejunas que hacen uso del objeto 'built-in' como esta Number
+ * pueden ser llamadas con el new delante de ellas, y eso convierte ese llamada a función en una
+ * llamada al 'constructor'.
+ *
+ *  IMPORTANTÍSIMA , PERO SUTÍL DIFERENCIA:
+ *  -Realmente no hay nada que sea una función constructor, en puesto de eso lo que hay
+ *   son simples llamadas a función.
+ *
+ * Cuando una función es invocada con el new delante, aka 'Constructor Call' las siguientes
+ * cosas pasan automáticamente:
+ *
+ *  1. Se crea un objeto totalmente nuevo.
+ *  2. El nuevo objeto es [[Prototype]]-linkeado. //De esto ni zorra aun ya que es del cap 3.
+ *  3. El nuevo objeto que se ha creado , es setteado en el bindeo del 'this' para esa llamada de función.
+ *  4. Hasta que la función ,
+ *
+
+ */
+
+
 //uff clases , un pequeño ejemplo ya que habla del class pattern
 // y el new estando aburrido de verlas , algo que no sucede.
 class miClase {
