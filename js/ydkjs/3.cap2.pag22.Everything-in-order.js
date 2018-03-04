562bcba78c6ba52a5d602f713b10f114d7232310
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-03-04 23:04:07 +0100

    hasta la pista. comentado primer snippet.

--- js/ydkjs/3.cap2.pag22.Everything-in-order.js
+++ js/ydkjs/3.cap2.pag22.Everything-in-order.js
@@ -34,21 +34,64 @@
  *  Cual es la más más prededencia tiene? el 'implicit binding' o el 'explicit binding' ?
  *  Testeemoslo:
  */
-function foo() {
-    console.log('this.a', this.a );//TODO: borrame.
-}
-var obj1 = {
- a:2,
- foo: foo
-};
+// function foo() {
+//     console.log('this.a', this.a);//TODO: borrame.
+// }
+//
+// var obj1 = {
+//     a: 2,
+//     foo: foo
+// };
+//
+// var obj2 = {
+//     a: 3,
+//     foo: foo
+// };
+//
+// obj1.foo();
+// obj2.foo();
+//
+// obj1.foo.call(obj2);
+// obj2.foo.call(obj1);
 
-var obj2 = {
-    a:3,
+/**
+ * Así que, el 'explicit binding' tiene precedencia frente al 'implicit binding',
+ * lo cual significa que te debes preguntar primero si se ha aplicado algún 'explicit binding'
+ * antes de mirar si hay 'implicit binding'
+ *
+ * Sólo nos queda descifrar ahora , donde encajaría la precedencia del 'new binding':
+ */
+
+function foo(algo) {
+   this.a = algo;
+}
+
+var obj1 = {
     foo: foo
 };
 
-obj1.foo();
-obj2.foo();
+var obj2 = {};
 
-obj1.foo.call(obj2);
-obj2.foo.call(obj1);
+obj1.foo(2);
+console.log('obj1.a', obj1.a);//2
+
+obj1.foo.call(obj2, 3);
+console.log('obj2.a', obj2.a);//3
+
+var bar = new obj1.foo(4);
+console.log('obj1.a', obj1.a);//2
+console.log('bar.a', bar.a);//4
+
+/**
+ * OK, el 'new binding' tiene mayor precedencia que el 'implicit binding', pero tu que piensas
+ * que tiene más o menos precedencia que el 'explicit binding'?
+ *
+ * Pista: ni el new in el call/apply pueden ser usadas conjuntamente , por lo que algo como
+ * foo.call(obj) no está permitido para testear el 'new binding' con el 'explicit binding' Pero aún así ,
+ * podemos usar el 'hard binding' para testear la precedencia de ambas reglas.
+ *
+ */
+
+
+
+
