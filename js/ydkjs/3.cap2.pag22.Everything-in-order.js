51923ed9b6642cd8ab3689300347cdf0e46cce27
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-03-04 20:41:09 +0100

    Every Thing in order

---
+++ js/ydkjs/3.cap2.pag22.Everything-in-order.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018 , a 4/03/18 20:35. GMU Gpl , la Licencia Murciana De codigo fuente Libre.
+ *
+ * Puedes hacer lo que te salga del pijo con este código, partiendo de eso :
+ *
+ * 1. Puedes copiarlo y publicarlo.
+ *         Pero siempre con esta licencia también e indicando el autor original si es que lo tiene
+ *         y de donde lo has sacao si se puede.
+ * 2. Puedes Ñapearlo, ampliarlo y modificarlo a tu antojo.
+ *        ¨Lo suyo sería¨ que entonces indicaras que lo has hecho y donde,
+ *         añadiendote como autor si quieres.
+ *
+ * Ni el autor original , ni los posteriores son responsables de los daños que este código pueda ocasionar.
+ * El autor también tiene derecho a ser totálmente anónimo y no ha de por que constar por ningún lao si le apetece.
+ *
+ *
+ */
+
+
+/**
+ *  Traducción ydkjs de kyle sympson libro 3 , capitulo 2 , pag 22. Everything in order.
+ *
+ *  Así, ahora hemos dejado al descubierto  las 4 reglas para el 'bindeo del this' en función de
+ *  quien hace la llamada.
+ *
+ *
+ *
+ */
