00d7c3d3338044128757b30ed5c431f0447ca5d5
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-03-04 22:33:42 +0100

    1 trozo con snippet incluido.

--- js/ydkjs/3.cap2.pag22.Everything-in-order.js
+++ js/ydkjs/3.cap2.pag22.Everything-in-order.js
@@ -21,8 +21,34 @@
  *  Traducción ydkjs de kyle sympson libro 3 , capitulo 2 , pag 22. Everything in order.
  *
  *  Así, ahora hemos dejado al descubierto  las 4 reglas para el 'bindeo del this' en función de
- *  quien hace la llamada.
+ *  quien hace la llamadas.
  *
+ *  Sólo necesitas es encontrar el call-site e inspeccionarlo para ver qué regla se aplica.
+ *  Pero qué pasa si el call-site tiene diferentes reglas de elección?
+ *  Tiene que existir un orden de precedencia entre esas reglas. Así que lo siguiente deja ver
+ *  como se aplica la prededencia de dichas reglas.
  *
+ *  Debe quedar claro que el ´Default binding´ es la regla de prioridad más baja de las cuatro.
+ *  Así que simplemente dejaremos eso a un lado.
  *
+ *  Cual es la más más prededencia tiene? el 'implicit binding' o el 'explicit binding' ?
+ *  Testeemoslo:
  */
+function foo() {
+    console.log('this.a', this.a );//TODO: borrame.
+}
+var obj1 = {
+ a:2,
+ foo: foo
+};
+
+var obj2 = {
+    a:3,
+    foo: foo
+};
+
+obj1.foo();
+obj2.foo();
+
+obj1.foo.call(obj2);
+obj2.foo.call(obj1);
