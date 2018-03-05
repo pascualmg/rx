ea5a8327b2d95666692d335b529a14b2b965c2b3
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-03-05 20:05:46 +0100

    fin determining this, pag movido de file. Etnra en el Everyting-in-order.

--- js/ydkjs/3.cap2.pag22.Everything-in-order.js
+++ js/ydkjs/3.cap2.pag22.Everything-in-order.js
@@ -213,3 +213,40 @@ var baz = new bar("p2");
 
 console.log('baz.val', baz.val);//p1p2 (la cagaste bien si esto pasa).
 
+
+/**
+ * DETERMINING 'this'
+ *
+ * Ahora , podemos resumir las reglas que determinan el 'this' de un 'call-site' de una función
+ * según su orden de precedencia. Haz estas preguntas in este orden y párate en la primera
+ * regla que se aplique:
+ */
+
+// 1. Se ha llamado a la función con un new ('new-binding')? Si es así entonces el 'this'
+//    es el objeto recién construido.
+var bar = new foo();
+
+
+// 2. Se ha llamado a la función con un call o apply ('explicit-binding'), incluso oculto
+//  desde una bind ('hard-binding')? Si es así entonces el 'this' es ese objeto indicado
+//  explíticamente.
+
+var bar = foo.call(obj);
+
+// 3. Se ha llamado a la función con un contexto ('explicit-binding')?
+//    también conocido como un 'owning' o 'containing object'? Si es que sí , entonces el 'this' es
+//    ese objeto de contexto.
+
+var bar = obj1.foo();
+
+// 4 De otra forma, por defecto el 'this' (default-binding) .
+// Si está en modo estricto 'this' es undefined.
+// En caso contrario, 'this' es el global-object.
+
+var bar = foo();
+
+/**
+ * Y así es , Eso es todo lo que hace falta para entender las reglas del 'this-binding' para
+ * las llamadas a función normales. bueno... casi ya que hay algunas excepciones.
+ */
+
